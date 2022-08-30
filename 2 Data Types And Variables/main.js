9a86a8d1c515eb9737d2294aa8c88ada210149e0
Ahmed zena <20812018100841@eng.zu.edu.eg>
2022-08-30 09:56:00 +0200

    second assignment js

---
+++ 2 Data Types And Variables/main.js
@@ -0,0 +1,70 @@
+// first question
+
+var numberOne;
+var numberTwo;
+numberOne = 10;
+numberTwo = 20;
+
+console.log(numberOne + "" + numberTwo);
+console.log("نوع البيانات  " + numberOne + "" + numberTwo);
+
+// console.log(`string text line 1
+// string text line 2`);
+
+// console.log('string text line 1\n' +
+// 'string text line 2');
+
+console.log(`${numberOne}${numberTwo}`);
+console.log(`${numberOne}${numberTwo} نوع البيانات`);
+// console.log (20  /n + 10 );
+
+// console.log("ahmed gamal \n zena");
+// console.log(numberTwo ,"/n" , numberOne);
+
+// let string =  + numberOne;
+// console.log (string);
+// console.log({ numberTwo: 1 }, '\n', { numberOne: 3 });
+// هي كدا بتضيف فالاول علامتين تنصيص فارغين وبتظبط
+console.log("", numberTwo, "\n", numberOne);
+
+console.log(`${numberTwo}
+${numberOne}`);
+
+// second question
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+
+// third question
+console.log("", numberTwo, "\n", numberOne);
+
+console.log(
+  "",
+  " `I'm In",
+  "\n",
+  "\\\\",
+  "\n",
+  "Love",
+  "\\\\",
+  '"""',
+  "'''",
+  "\n",
+  "++",
+  "With",
+  " ++",
+  "\n",
+  '\\"""',
+  '\\"""',
+  "\n",
+  ' ""JavaScript""``  '
+);
+
+// fourth question
+
+let a = 21;
+let b = 20;
+let c = `${b}${a}`;
+console.log(c);
+// console.log( _ a,"_" ,c ,"_" ,c,"_" ,c );
+console.log(`_${a} `);
+
+console.log(`_${a}_${c}_${c}_${c}_${b}_`);
