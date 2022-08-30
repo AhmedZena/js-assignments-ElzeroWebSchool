64240b48736aa3d89afe2aa6a0c212d9b03af6e1
Ahmed zena <20812018100841@eng.zu.edu.eg>
2022-08-30 09:43:52 +0200

    first assignment js

---
+++ 1 Introduction/main.js
@@ -0,0 +1,53 @@
+//Assignment 1
+// Done نظري
+
+//Assignment 2
+document.write("<h1>Elzero </h1>");
+window.onload = function () {
+  document.querySelector("h1").style.color = "Blue";
+  document.querySelector("h1").style.fontSize = "80px";
+  document.querySelector("h1").style.fontFamily = "Arial";
+  document.querySelector("h1").style.textAlign = "center";
+  document.querySelector("h1").style.fontWeight = "bold";
+};
+
+//Assignment 3
+console.log(
+  "%c Elzero %c web %c school",
+  "color:red ; font-size:40px",
+  "color:green ; font-size:40px",
+  "color:blue ; font-size:40px"
+);
+
+//Assignment 4
+console.group("Group 1");
+console.log("message 1");
+console.log("message 2");
+console.group("child group");
+console.log("message 1");
+console.log("message 2");
+console.group("Grand child group");
+console.log("message 1");
+console.log("message 2");
+console.groupEnd();
+console.groupEnd();
+console.groupEnd();
+console.group("Group 2");
+console.log("message 1");
+console.log("message 2");
+
+//Assignment 5
+
+// print inline
+console.log(["Elzero", "ahmed", "sameh", "gamal", "Aya"]);
+// print in table
+console.table(["Elzero", "ahmed", "sameh", "gamal", "Aya"]);
+
+//Assignment 6
+
+/* console.log("Iam In Console");
+ document.write("Iam In Page");
+  */
+
+//  console.log("Iam In Console");
+// document.write("Iam In Page");
