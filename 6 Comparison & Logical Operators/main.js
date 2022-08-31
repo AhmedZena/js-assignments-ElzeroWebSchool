f41a670547c57cec92a7a8e15864057007878dc7
Ahmed zena <20812018100841@eng.zu.edu.eg>
2022-08-31 20:51:39 +0200

    the six assignment

---
+++ 6 Comparison & Logical Operators/main.js
@@ -0,0 +1,39 @@
+//first Assignment
+console.log(" first Assignment"); // true
+
+console.log(100 == "100"); // true
+console.log(100 < 1000); // true
+console.log(110 > 100 && 10 < 20); // true
+console.log(-10 == "-10"); // true
+console.log(!-50 >= +"-40"); // true
+console.log(!10 <= -"-40"); // true
+console.log(+"10" >= 10); // true
+console.log(-20 < false); // true
+
+//second Assignment
+
+console.log(" second Assignment"); // true
+
+let num1 = 10;
+let num2 = 20;
+
+console.log(10 < 20); // true
+console.log(10 != 20); // true
+console.log(!10 != 20); // true
+console.log(10 <= 20); // true
+console.log(-10 <= 20); // true
+console.log(-10 < 20); // true
+console.log(10 >= -20); // true
+console.log(10 > -20); // true
+
+//third Assignment
+
+console.log(" third Assignment"); // true
+
+let a = 20;
+let b = 30;
+let c = 10;
+
+console.log((a < b && a > c) || a >= b); // true
+console.log(a <= b > a < c); // true
+console.log(!(a > b) && !(a > b) && !(a < c) && !(a < c)); // true
