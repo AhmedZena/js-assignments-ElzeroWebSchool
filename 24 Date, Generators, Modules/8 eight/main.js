d8aec0fb70f15a72c425c12d05399bce585e5936
Ahmed Zena <ahmedzena53@gmail.com>
2022-09-27 23:31:52 +0200

    ..

---
+++ 24 Date, Generators, Modules/8 eight/main.js
@@ -0,0 +1,7 @@
+// main.js File
+import calc from "./mod-one.js";
+import modOne from "./mod-two.js";
+
+console.log("i am main.js");
+console.log(calc(modOne.numOne, modOne.numTwo, modOne.numThree)); // 60
+console.log("-".repeat(50));
