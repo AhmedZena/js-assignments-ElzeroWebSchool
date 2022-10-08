afa1cf98b657fe4065a8e25f0aa23f7eaf2852d7
Ahmed Zena <ahmedzena53@gmail.com>
2022-10-08 23:14:59 +0200

    .

---
+++ 24 Date, Generators, Modules/8 eight/mod-two.js
@@ -0,0 +1,17 @@
+// mod-two.js File
+
+let a = 10; // Do Not Edit Names
+let b = 20; // Do Not Edit Names
+let c = 30; // Do Not Edit Names
+
+let modOne = {
+  numOne: a,
+  numTwo: b,
+  numThree: c,
+};
+export default modOne;
+
+console.log("-".repeat(50));
+console.log("i am mod-two.js");
+console.log(modOne);
+console.log("-".repeat(50));
